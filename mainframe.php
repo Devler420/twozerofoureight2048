3ec00e8872fe1559b563ff502176126636e59c3b
Panupong Boom Utvichai <boompanupong@gmail.com>
2021-05-15 03:28:24 +0700

    first commit

---
+++ mainframe.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2048</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="cal.js"></script>
+</head>
+<body>
+    <div class="mainframe">
+        <div class="box" id="x0,0" style="top: 10px; left: 10px;"></div>
+        <div class="box" id="x0,1" style="top: 10px; left: 70px;"></div>
+        <div class="box" id="x0,2" style="top: 10px; left: 130px;"></div>
+        <div class="box" id="x0,3" style="top: 10px; left: 190px;"></div>
+        <div class="box" id="x1,0" style="top: 70px; left: 10px;"></div>
+        <div class="box" id="x1,1" style="top: 70px; left: 70px;"></div>
+        <div class="box" id="x1,2" style="top: 70px; left: 130px;"></div>
+        <div class="box" id="x1,3" style="top: 70px; left: 190px;"></div>
+        <div class="box" id="x2,0" style="top: 130px; left: 10px;"></div>
+        <div class="box" id="x2,1" style="top: 130px; left: 70px;"></div>
+        <div class="box" id="x2,2" style="top: 130px; left: 130px;"></div>
+        <div class="box" id="x2,3" style="top: 130px; left: 190px;"></div>
+        <div class="box" id="x3,0" style="top: 190px; left: 10px;"></div>
+        <div class="box" id="x3,1" style="top: 190px; left: 70px;"></div>
+        <div class="box" id="x3,2" style="top: 190px; left: 130px;"></div>
+        <div class="box" id="x3,3" style="top: 190px; left: 190px;"></div>
+    </div>
+</body>
+</html>
